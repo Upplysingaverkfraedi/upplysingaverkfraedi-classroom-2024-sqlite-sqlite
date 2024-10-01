fe73ff9ea25d6ad26a9cd546cddd19a5e19b63ad
thrainn25 <taa9@hi.is>
2024-10-01 12:36:52 +0000

    Breytingar á spurningu 1
    Sjá hér breytingar og lagfæringu á spurningu 1. Lagaði bæði kóðann fyrir fyrstu spurninguna og setti betri lýsingu fyrir allar þrjár spurningarnar.

--- code/names.sql
+++ code/names.sql
@@ -1,5 +1,5 @@
 -- Output file
--- Create the table for storing name frequencies
+-- Búa til töflu til að geyma nafna fjöldann
 CREATE TABLE IF NOT EXISTS names (
     name TEXT NOT NULL,
     year INTEGER NOT NULL,
@@ -8,30 +8,39 @@ CREATE TABLE IF NOT EXISTS names (
     PRIMARY KEY (name, year, type)
 );
 
--- Load data into the table
-.mode csv
+-- Setja gögnin í töflu
+-- Merkja að ég sé að lesa inn gögn frá csv skrám
+.mode csv 
+-- importa gögnin
 .import 'data\first_names_freq.csv' names
-UPDATE names SET type = 'first_name' WHERE type IS NULL;
+-- Setja öll gögn fyrir "first_name" sem first_names
+UPDATE names SET type = 'first_name' WHERE type IS NULL; 
 
+-- Gera það sama við seinni skránni nema middle_name
 .import 'data\middle_names_freq.csv' names
 UPDATE names SET type = 'middle_name' WHERE type IS NULL;
 
--- Output file
+-- Output file sem texta skrá
 .output names_output.txt
 
--- Analysis Queries:
+-- Svör á spurningum:
 
 -- Spurning 1
+-- Prenta út spurninguna 
 SELECT 'Spurning 1: Hvaða hópmeðlimur á algengasta eiginnafnið??' AS question;
-SELECT name, MAX(frequency) AS max_frequency
+-- Setja niðurstöðurnar sem summa af öllu count eftir nöfnunum og prenta út hvaða nafn hefur flest count s.s. er alngengast
+SELECT name, SUM(frequency) AS total_frequency
 FROM names
+-- Leita af eignarnöfnunum okkar þrem og prenta út svarið með fjöldanum af count fyrir það nafn sem er alngengast
 WHERE type = 'first_name' AND name IN ('Þráinn', 'Ásdís', 'Gígja')
 GROUP BY name
-ORDER BY max_frequency DESC
+ORDER BY total_frequency DESC
 LIMIT 1;
 
 -- Spurning 2
+-- Prenta út spurninguna
 SELECT 'Spurning 2: Hvenær voru öll nöfnin vinsælust?' AS question;
+-- Þessi kóði les bæði fyrstu og milli nöfnin okkar og skilar þeirri línu (row) sem hefur flest count s.s. hvenær hvert nafn var vinsælast
 SELECT name, year
 FROM (
     SELECT name, year, frequency,
@@ -47,13 +56,16 @@ FROM (
 WHERE row_num = 1;
 
 -- Spurning 3
+-- Prenta út spurninguna
 SELECT 'Spurning 3: Hvenær komu þau fyrst fram?' AS question;
+-- Þessi kóði finnur minnsta árið sem er við hvert nafn og skilar svo nafninu með því ártali
 SELECT name, MIN(year) AS first_appearance
 FROM names
+-- Hér er verið að segja hvar á að leita eftir hvaða nafni s.s. í hvaða skrá
 WHERE (type = 'first_name' AND name IN ('Ásdís', 'Gígja', 'Þráinn'))
    OR (type = 'middle_name' AND name IN ('Marín', 'Ágúst'))
 GROUP BY name
 ORDER BY first_appearance;
 
--- Reset output mode
+-- Endurstilla output mode
 .output stdout
