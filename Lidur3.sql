e672ce444290b00591b5455fd7ab65c024125a39
AldaAsl <aau2@hi.is>
2024-10-03 23:33:44 +0000

    lidur3_uppfærdur

--- Lidur3.sql
+++ Lidur3.sql
@@ -2,32 +2,32 @@
 CREATE TABLE IF NOT EXISTS hlaup (
     id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Auðkenni hlaupsins
     upphaf DATETIME NOT NULL,              -- Tímasetning upphafs hlaups
-    endir DATETIME,                        -- Áætluð lok hlaups
+    endir DATETIME NOT NULL,               -- Áætluð lok hlaups
     nafn TEXT NOT NULL,                    -- Nafn hlaupsins
-    fjoldi INTEGER                         -- Fjöldi þátttakenda
+    fjoldi INTEGER NOT NULL                -- Fjöldi þátttakenda
 );
 
 -- 2. Búa til töflu fyrir timataka
 CREATE TABLE IF NOT EXISTS timataka (
     id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Auðkenni keppanda
-    hlaup_id INTEGER,                      -- Ytri lykill sem vísar í hlaup töfluna
+    hlaup_id INTEGER NOT NULL,             -- Ytri lykill sem vísar í hlaup töfluna
     nafn TEXT NOT NULL,                    -- Nafn keppanda
     timi TEXT NOT NULL,                    -- Tími sem keppandi lauk hlaupinu á
     kyn TEXT,                              -- Kyn keppanda (ef gefið upp)
     aldur INTEGER,                         -- Aldur keppanda (ef gefið upp)
-    UNIQUE(hlaup_id, nafn, timi),          -- Kemur í veg fyrir tvítöku gagna
+    UNIQUE(hlaup_id, nafn),                -- Kemur í veg fyrir tvítöku gagna
     FOREIGN KEY (hlaup_id) REFERENCES hlaup(id)
 );
 
 -- 3. Setja inn hlaup og keppendur (þú getur bætt við fleiri)
-INSERT INTO hlaup (upphaf, endir, nafn, fjoldi) 
-VALUES ('2022-08-28 10:00:00', '2022-08-28 12:00:00', 'Íslandsmót í criterium 2022', 150);
+INSERT INTO hlaup (upphaf, endir, nafn, fjoldi)
+VALUES ('2022-08-28 10:00:00', '2022-08-28 12:00:00', 'Íslandsót í criterium 2022', 1);
 
-INSERT INTO timataka (hlaup_id, nafn, timi, kyn, aldur) 
-VALUES 
-(1, 'Jon Jonsson', '00:35:21', 'Karl', 25),
-(1, 'Sara Sigurðardóttir', '00:40:15', 'Kona', 30),
-(1, 'Arnar Arnalds', '00:29:05', 'Karl', 22);
+INSERT INTO timataka (hlaup_id, nafn, timi, kyn, aldur)
+VALUES
+(13, 'Jon Jonsson', '00:35:21', 'Karl', 25),
+(13, 'Sara Sigurðardóttir', '00:40:15', 'Kona', 30),
+(13, 'Arnar Arnalds', '00:29:05', 'Karl', 22);
 
 -- 4. Sannreyna fjölda keppenda í hverju hlaupi
 SELECT h.nafn AS hlaup, 
